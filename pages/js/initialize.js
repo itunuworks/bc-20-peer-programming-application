a8e8824c6054809c77d467ace037f3a43f195d2c
Itunu Fatoki <itunuworks@gmail.com>
2017-03-22 19:21:07 +0100

    Setup js code for the Main page

--- pages/js/initialize.js
+++ pages/js/initialize.js
@@ -1,84 +1,84 @@
 'use strict';
 
 function initializeFirebase(){
-	var config = {
-  	apiKey: "AIzaSyBDV1xH1vjy5e828BKvsBhFwDhbDiyyFEk",
-	  authDomain: "peer-programming-application.firebaseapp.com",
-	  databaseURL: "https://peer-programming-application.firebaseio.com",
-	  storageBucket: "peer-programming-application.appspot.com",
-	  messagingSenderId: "1058474997451"
+  var config = {
+    apiKey: "AIzaSyBDV1xH1vjy5e828BKvsBhFwDhbDiyyFEk",
+    authDomain: "peer-programming-application.firebaseapp.com",
+    databaseURL: "https://peer-programming-application.firebaseio.com",
+    storageBucket: "peer-programming-application.appspot.com",
+    messagingSenderId: "1058474997451"
   };
   firebase.initializeApp(config);
   console.log('fireBase just got initialized.');
 }
 
 function AuthApp(signInButton, email, password){
-	
-	this.initFireBase();
+  
+  this.initFireBase();
 
-	this.signInButton = signInButton;
-	this.email = email;
-	this.password = password;
-	this.firstTime = true;	
-	console.log(this.signInButton);
-	if (this.signInButton != null){
-		this.signInButton.addEventListener ('click', this.signIn.bind(this));
-	}
+  this.signInButton = signInButton;
+  this.email = email;
+  this.password = password;
+  this.firstTime = true;  
+  console.log(this.signInButton);
+  if (this.signInButton != null){
+    this.signInButton.addEventListener ('click', this.signIn.bind(this));
+  }
 }
 
 AuthApp.prototype.initFireBase = function(){
-	this.auth = firebase.auth();
-	this.database = firebase.database();
-	
-	this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
+  this.auth = firebase.auth();
+  this.database = firebase.database();
+  
+  this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
 };
 
 AuthApp.prototype.signUp = function (event){
-	event.preventDefault();
-	var user = this.auth.createUserWithEmailAndPassword(this.email.value, this.password.value);
-	
-	console.log(user);
-	user.catch(function(error){
-		var errorCode = error.code;
-		var errorMessage = error.message;
-		if (errorMessage == 'auth/weak-password'){
-			alert('The password is too weak.');
-		}
-		else{
-			alert(errorMessage);
-		}
-		console.log(error);
-	});
+  event.preventDefault();
+  var user = this.auth.createUserWithEmailAndPassword(this.email.value, this.password.value);
+  
+  console.log(user);
+  user.catch(function(error){
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    if (errorMessage == 'auth/weak-password'){
+      alert('The password is too weak.');
+    }
+    else{
+      alert(errorMessage);
+    }
+    console.log(error);
+  });
 }
 
 AuthApp.prototype.signIn = function (event){
-	console.log('Sign In just got called.');
-	event.preventDefault();
+  console.log('Sign In just got called.');
+  event.preventDefault();
 
-	var user = this.auth.signInWithEmailAndPassword(this.email.value, this.password.value);
-	
-	user.catch(function(error){
-		var errorCode = error.code;
-		var errorMessage = error.message;
-		alert(errorMessage);
-	});
+  var user = this.auth.signInWithEmailAndPassword(this.email.value, this.password.value);
+  
+  user.catch(function(error){
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    alert(errorMessage);
+  });
 }
 
 AuthApp.prototype.signOut = function(){
-	console.log("sign out called");
-	this.auth.signOut();
+  console.log("sign out called");
+  this.auth.signOut();
 }
 
 AuthApp.prototype.onAuthStateChanged = function(user){
-	console.log(user);
-	console.log("onAuthStateChanged is executed");
+  console.log(user);
+  console.log("onAuthStateChanged is executed");
 
-	if (user){
-		console.log(user.email + ' is signed in.');
-		window.alert('Signed in ' + user.email);
-		window.location.href = "landingpage";
-	}
-	else{
-		console.log('No user signed in');
-	}
+  if (user){
+    console.log(user.email + ' is signed in.');
+    window.alert('Signed in ' + user.email);
+    window.location.href = "landingpage";
+  }
+  else{
+    console.log('No user signed in');
+  }
 }
